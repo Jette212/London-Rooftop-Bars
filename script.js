02cd063d802354614a84f6249533c2b0dcd88996
Furnisha <furnisha_lycett@hotmail.co.uk>
2017-03-05 20:46:47 +0000

    Added title above videos and edited css

--- script.js
+++
@@ -1,5 +0,0 @@
-  $('#read-more').on('click', function() {
-    $('#read-more-collapse').toggleClass("uncollapsed");
-    $('#read-more-plus').toggleClass("uncollapsed");
-    $('#read-more-minus').toggleClass("uncollapsed");
-  });
